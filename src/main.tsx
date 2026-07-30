a854d1b771b05926550582c1f2fe56d90ad6f05b
Josh Jones <thejoshjones1@gmail.com>
2026-07-30 00:30:05 -0700

    refactor: separate router and route definitions

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'next-themes'
-import App from './App'
+import { AppRouter } from './router'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AppRouter />
     </ThemeProvider>
   </StrictMode>,
 )
