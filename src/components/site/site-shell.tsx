76031f3fb85d75cdd1f2ae961023a1681c74b9ff
David Coelho <davidkfco@gmail.com>
2026-08-06 13:53:46 -0300

    Adding prefetch on hover/tap/keyboard tab. Added chunking to fetch all pages mdx files in one request (this has no size limit for now, but can easily be added)

--- src/components/site/site-shell.tsx
+++ src/components/site/site-shell.tsx
@@ -3,10 +3,14 @@ import { useLocation } from "react-router"
 
 import { FailoverNotice } from "@/components/site/failover-notice"
 import { SiteHeader } from "@/components/site/site-header"
+import { useLinkPrefetch } from "@/hooks/use-link-prefetch"
 import { guideCatalog } from "@/lib/content"
 
 function SiteShell({ children }: PropsWithChildren) {
   const { pathname } = useLocation()
+
+  useLinkPrefetch()
+
   const normalizedPathname =
     pathname === "/" ? pathname : pathname.replace(/\/+$/, "")
   const isLandingPage = normalizedPathname === "/"
